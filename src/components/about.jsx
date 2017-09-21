83c9785fa14032c909d93cf36ba9bb51dcc1dd50
Julianne Costa <julesleighcosta@gmail.com>
2017-09-21 13:57:48 -0700

    Edit minor styling details and add resume link

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,7 +6,7 @@ const About = () => (
     <section className="content-wrapper">
       <div className="contact-info">
         <p><i className="fa fa-phone" aria-hidden="true"></i>973.580.7876</p>
-        <p>julesleighcosta@gmail.com</p>
+        <p><i className="fa fa-envelope-o" aria-hidden="true"></i>julesleighcosta@gmail.com</p>
       </div>
       <p>Hey, I'm Jules. My core values are honest communication, compassion, purpose-driven learning and growth, self-care, creativity, and nurturing the growth of people around me.</p>
       <p>I'm a software engineer based in San Francisco. When I'm not coding you can find me riding my fixed-gear around the city, handpoking tattoos on friends, or most likely snuggling my cat, Kevin.</p>
