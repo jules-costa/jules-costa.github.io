ceb13dd2398369f3fcba7222f3fed6e1805ea150
Julianne Costa <julesleighcosta@gmail.com>
2017-09-24 16:24:19 -0700

    Edit about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,7 +10,7 @@ const About = () => (
       </div>
       <p>Hey, I'm Jules. My core values are honest communication, compassion, purpose-driven learning and growth, self-care, creativity, and nurturing the growth of people around me.</p>
       <p>I'm a software engineer based in San Francisco. When I'm not coding you can find me riding my fixed-gear around the city, handpoking tattoos on friends, or most likely snuggling my cat, Kevin.</p>
-      <p>My technical toolbox includes: Ruby, Rails, Redux, React.js, ES5, ES6, Postgres, SQL, HTML5, CSS3, SASS, Webpack/Babel & Heroku.</p>
+      <p>My technical toolbox includes: Ruby, Rails, Redux, React.js, ES5/ES6, Postgres, SQL, HTML5, CSS3, SASS, Webpack/Babel & Heroku.</p>
       <p>I love discovering new ways to design and develop software. I have a keen eye for detail, and strive to produce clean, maintainable, scaleable code.</p>
       <p>I'm currently seeking opportunities in the Bay Area. Let's connect!</p>
     </section>
