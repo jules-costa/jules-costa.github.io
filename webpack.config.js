b80e48c204abe12e770c64c1f281d6804c98ed47
Julianne Costa <julesleighcosta@gmail.com>
2017-10-09 14:52:31 -0700

    Remove unnecessary css

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,6 @@
 var path = require("path");
 var webpack = require("webpack");
 
-var plugins = []; // if using any plugins for both dev and production
-var devPlugins = []; // if using any plugins for development
-
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
-
 module.exports = {
   context: __dirname,
   entry: "./src/index.js",
@@ -28,7 +8,6 @@ module.exports = {
     path: path.resolve(__dirname),
     filename: "./public/bundle.js"
   },
-  plugins: plugins,
   module: {
     loaders: [
       {
