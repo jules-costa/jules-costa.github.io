cb3f064a02fa8d60d32fb52e7be199d2f920beda
Julianne Costa <julesleighcosta@gmail.com>
2017-11-02 10:27:28 -0700

    Add technologies component

---
+++ src/components/technologies.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Technologies = () => (
+  <div className="wrapper">
+    <h2 className="header">TECHNOLOGIES</h2>
+    <section className="content-wrapper">
+      <ul>
+        <li>Ruby</li>
+        <li>Rails 4/5</li>
+        <li>SQL 4/5</li>
+        <li>Postgres</li>
+        <li>ES5/ES6</li>
+        <li>jQuery</li>
+        <li>ReactJS</li>
+        <li>Redux</li>
+        <li>RSpec</li>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>Webpack</li>
+        <li>Babel</li>
+        <li>Heroku</li>
+        <li>Netlify</li>
+      </ul>
+    </section>
+  </div>
+);
+
+export default About;
